ec398fee7dd95b764b9ad6d715f3207004c593df
Stephane Loppinet <stephaneloppinet@gmail.com>
2025-01-26 15:52:14 +0100

    undo opti binarytree because regression

--- src/binarytree.adb
+++ src/binarytree.adb
@@ -183,20 +183,28 @@ package body BinaryTree is
                               ActionCallback : not null access procedure (ABR : in out T_BinaryTree; Parent : in out T_BinaryTree; Stop : in out Boolean);
                               Stop : in out Boolean) is
    begin
+      -- Si l'arbre est vide ou si le flag Stop est à True, on arrête immédiatement
+      if ABR = null or else Stop then
+         return;
+      end if;
 
       -- Appliquer la fonction de traitement sur le nœud courant
       ActionCallback(ABR, Parent, Stop);
 
-      -- Si l'arbre est vide ou si le flag Stop est à True, on arrête immédiatement
-      if ABR = null or Stop then
+      -- Si le parcours doit s'arrêter, on ne continue pas
+      if Stop then
          return;
       end if;
 
       -- Parcours du sous-arbre gauche si présent et si le flag Stop n'est pas à True
-      traverseTreeAndApply(ABR.all.Left, ABR, ActionCallback, Stop);
+      if ABR.all.Left /= null then
+         traverseTreeAndApply(ABR.all.Left, ABR, ActionCallback, Stop);
+      end if;
 
       -- Parcours du sous-arbre droit si présent et si le flag Stop n'est pas à True
-      traverseTreeAndApply(ABR.all.Right, ABR, ActionCallback, Stop);
+      if ABR.all.Right /= null then
+         traverseTreeAndApply(ABR.all.Right, ABR, ActionCallback, Stop);
+      end if;
    end traverseTreeAndApply;
 
    function getKey (ABR : T_BinaryTree) return Integer is
